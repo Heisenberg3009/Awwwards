a3ef24edbbdfaa11353ac86312f167b9c2b2788c
Tulsi Narayanan <tulsinarayanan33@gmail.com>
2024-11-25 08:28:10 +0530

    Tenth Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 
 import Hero from "./components/Hero.jsx";
+import About from "./components/About.jsx";
 
 const App = () => {
     return (
         <main className="relative min-h-screen w-screen overflow-x-hidden">
          <Hero />
+         <About />
+
+         <section className="z-0 min-h-screen w-screen overflow-x-hidden "/>
+
+
+
+
+
         </main>
     )
 }
